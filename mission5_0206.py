b46c00ca1ac8966ec46fc35def7a806954950d92
chae minkyoung <chea220215@gmail.com>
2023-02-07 00:05:43 +0900

    second

--- mission5_0206.py
+++ mission5_0206.py
@@ -1,65 +1,48 @@
-import random
-
-class Node:
+class Node():
     def __init__(self):
         self.data=None
         self.link=None
-
-def print_nodes(start):
-    current=start
-    if current == None:
+        self.reverse=None
+def print_node(start):
+    current = start
+    print("정방향 -->",end = ' ')
+    if current.link == None:
         return
-    print(current.data,end=' ')
-    while current.link != head:
-        current = current.link
-        print(current.data, end=' ')
-    print()
-def make_list(convenience):
-    global head, current, pre
-
+    while current.link!=None:
+        print(current.data, end= ' ')
+        current=current.link
+    print(current.data)
+def print_rnode(start):
+    current = start
+    print("역방향 -->",end = ' ')
+    if current.reverse == None:
+        return
+    while current.reverse!=None:
+        print(current.data, end= ' ')
+        current=current.reverse
+    print(current.data)
+if __name__=="__main__":
+    Array=['다현','정연','쯔위','사나','지효']
     node=Node()
-    node.data=convenience
+    node.data=Array[0]
+    head=node
+    current = node
+    for member in Array[1:]:
+        pre = node
+        node=Node()
+        current=node
+        pre.link=node
+        node.data=member
+    print_node(head)
+
+    node = Node()
+    node.data= Array[-1]
+    head=node
     current=node
-
-    if head==None:
-        head=node
-        head.link=head
-        return
-
-    if convenience[1] < head.data[1]:
-        node.link = head
-        last = head
-        while last.link != head:
-            last = last.link
-        last.link =node
-        head = node
-        return
-    current=head
-    while current.link != head:
-        pre = current
-        current = current.link
-        if convenience[1] < current.data[1]:
-            pre.link = node
-            node.link = current
-            return
-
-    current.link=node
-    node.link = head
-
-
-
-
-if __name__=='__main__':
-    head, current, pre = None,None,None
-    a = ['A', 'B', 'C', 'D', 'E']
-    conveniences = []
-    for i in range(len(a)):
-        x = random.randint(1, 100)
-        y = random.randint(1, 100)
-        b = (x ** 2 + y ** 2) ** (1 / 2)
-        c = (a[i], b)
-        conveniences.append(c)
-    print(conveniences)
-    for convenience in conveniences:
-        make_list(convenience)
-    print_nodes(head)
+    for member in Array[-2::-1]:
+        pre=node
+        node=Node()
+        current=node
+        pre.reverse=node
+        node.data=member
+    print_rnode(head)
--- mission6_0206.py
+++ mission6_0206.py
@@ -1,31 +1,26 @@
-colors=['주황','초록','파랑','보라','빨강','노랑']
 def push(data):
-    global size, top, stack
-    top +=1
+    global top, stack, size
     if top < size:
+        top+=1
         stack[top]=data
     else:
         return
 def pop():
-    global size,top,stack
+    global top,stack,size
     if top != -1:
         data = stack[top]
         stack[top]=None
         top-=1
         return data
-    else:
-        return
-if __name__ == "__main__":
+if __name__=="__main__":
+    poem='''진달래꽃\n나 보기가 역겨워 가실 때에는\n말없이 고이 보내드리오리다.'''
+    size = len(poem)
     top=-1
-    size=len(colors)
-    stack=[None for _ in range(len(colors))]
-    for color in colors:
-        push(color)
-    print("과자집에 가는 길 : ",end= ' ')
-    for st in stack:
-        print(f'{st} -->',end = ' ')
-    print('과자집')
-    print('우리집에 오는 길 : ',end=' ')
-    for _ in colors:
-        print(pop(),'-->',end=' ')
-    print('우리집')
+    stack=[None for _ in range(len(poem))]
+    print('-----원본-----')
+    for i in poem:
+        push(i)
+    print(''.join(stack))
+    print('-----거꾸로 처리된 결과-----')
+    for i in range(len(poem)):
+        print(pop(),end='')
